47ad3af7ac58857143142cdabdd289e01e08c4d9
David <admin@dxscx.com>
2018-07-26 09:35:33 +0800

    test: test dep

--- goutil.go
+++ goutil.go
@@ -1,5 +1,22 @@
 package goutil
 
-func Example() {
+import (
+	"goutil/timeutil/stopwatch"
+	"goutil/strutil"
+	"goutil/logutil"
+	"goutil/dbutil"
+	"goutil/dbutil/sqlutil"
+)
 
+func Example() {
+	//sw
+	stopwatch.NewStopWatch("test")
+	//fmt
+	strutil.Format("%s", "hello")
+	//console
+	logutil.Console.Println("heloo")
+	//db
+	dbutil.NewDB("", "", "", "", "")
+	//sql
+	sqlutil.Example()
 }

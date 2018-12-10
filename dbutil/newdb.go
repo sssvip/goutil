d8cc2a8ae6b3683bf293ae8d70a3be11f66f5f63
David <admin@dxscx.com>
2018-12-10 13:12:44 +0800

    feat: add sql statistic func

--- dbutil/newdb.go
+++ dbutil/newdb.go
@@ -3,9 +3,11 @@ package dbutil
 import (
 	"database/sql"
 	"github.com/sssvip/goutil/dbutil/sqlutil"
-	"github.com/sssvip/goutil/timeutil/stopwatch"
-	"github.com/sssvip/goutil/jsonutil"
 	"github.com/sssvip/goutil/logutil"
+	"github.com/sssvip/goutil/timeutil/stopwatch"
+	"goutil/strutil"
+	"sync"
+	"sync/atomic"
 )
 
 /*
@@ -14,10 +16,32 @@ import (
 
 */
 
+type SQLStatistic struct {
+	SQLStr                  string
+	MaxExecTimeMilliSeconds int64
+	MinExecMilliSeconds     int64
+	ExecCount               int32
+	ExecTotalTime           int64
+}
+
+func (sqlStatistic *SQLStatistic) ExecOnce(sqlStr string, useMilliSeconds int64) {
+	sqlStatistic.SQLStr = sqlStr
+	if sqlStatistic.MaxExecTimeMilliSeconds < useMilliSeconds {
+		sqlStatistic.MaxExecTimeMilliSeconds = useMilliSeconds
+	}
+	if sqlStatistic.MinExecMilliSeconds > useMilliSeconds {
+		sqlStatistic.MinExecMilliSeconds = useMilliSeconds
+	}
+	atomic.AddInt32(&sqlStatistic.ExecCount, 1)
+	atomic.AddInt64(&sqlStatistic.ExecTotalTime, useMilliSeconds)
+}
+
 type DBWrapper struct {
 	slowSQLSeconds   int
 	OriginDB         *sql.DB
 	openCheckSlowSQL bool
+	statisticSQL     bool
+	statistics       sync.Map
 }
 
 var slowSQLCheck = "slowSQLCheck"
@@ -30,18 +54,49 @@ func (db *DBWrapper) OpenCheckSlowSQL(open bool) {
 	db.openCheckSlowSQL = open
 }
 
-func (db *DBWrapper) check(sw *stopwatch.StopWatch, o interface{}) {
+func (db *DBWrapper) StatisticSQL(open bool) {
+	db.statisticSQL = open
+}
+
+func (db *DBWrapper) ClearStatistic() {
+	db.statistics = sync.Map{}
+}
+
+func (db *DBWrapper) Statistic() (stats []SQLStatistic) {
+	db.statistics.Range(func(key, value interface{}) bool {
+		stats = append(stats, *value.(*SQLStatistic))
+		return true
+	})
+	return
+}
+
+func (db *DBWrapper) checkSQLStr(sw *stopwatch.StopWatch, sql string) {
 	if sw == nil {
 		return
 	}
-	if db.openCheckSlowSQL && int(sw.ElapsedSeconds()) > db.slowSQLSeconds {
-		logutil.Warning.Println("found slow sql:", jsonutil.MarshalToString(o))
+	if db.openCheckSlowSQL {
+		execTime := sw.ElapsedMilliSeconds()
+		//统计sql
+		if db.statisticSQL {
+			key := strutil.Md5(sql)
+			v, ok := db.statistics.Load(key)
+			if !ok {
+				var stat SQLStatistic
+				stat.ExecOnce(sql, execTime)
+				db.statistics.Store(key, &stat)
+			} else {
+				v.(*SQLStatistic).ExecOnce(sql, execTime)
+			}
+		}
+		if int(execTime)*1000 > db.slowSQLSeconds {
+			logutil.Warning.Println("found slow sql:", sql)
+		}
 	}
 }
 
 func NewDB(username, password, address, port, database string) *DBWrapper {
 	return &DBWrapper{OriginDB: NewDBByArg(username, password, address, port, database),
-		slowSQLSeconds: 10, openCheckSlowSQL: false}
+		slowSQLSeconds: 10, openCheckSlowSQL: false, statisticSQL: false, statistics: sync.Map{}}
 }
 
 func (db *DBWrapper) QueryForObjectBySQLStr(sqlStr string, columnsAddress ...interface{}) error {
@@ -50,7 +105,9 @@ func (db *DBWrapper) QueryForObjectBySQLStr(sqlStr string, columnsAddress ...int
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	e := QueryForObjectBySQLStr(db.OriginDB, sqlStr, columnsAddress...)
-	db.check(t, sqlStr)
+	if db.openCheckSlowSQL {
+		db.checkSQLStr(t, sqlStr)
+	}
 	return e
 }
 
@@ -60,7 +117,10 @@ func (db *DBWrapper) QueryForObject(sqlGen *sqlutil.SQLGen, columnsAddress ...in
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	e := QueryForObject(db.OriginDB, sqlGen, columnsAddress...)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Query()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return e
 }
 
@@ -70,7 +130,10 @@ func (db *DBWrapper) CountBySQLGen(sqlGen *sqlutil.SQLGen) (result int, err erro
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = CountBySQLGen(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Count()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return result, err
 }
 
@@ -80,7 +143,9 @@ func (db *DBWrapper) CountBySQLStr(sqlStr string, args ...interface{}) (result i
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = CountBySQLStr(db.OriginDB, sqlStr, args...)
-	db.check(t, sqlStr)
+	if db.openCheckSlowSQL {
+		db.checkSQLStr(t, sqlStr)
+	}
 	return result, err
 }
 
@@ -90,7 +155,10 @@ func (db *DBWrapper) InsertTableBySQLGen(sqlGen *sqlutil.SQLGen) (result int64, 
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = InsertTableBySQLGen(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Insert()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -100,7 +168,10 @@ func (db *DBWrapper) InsertTableBySQLGenTx(tx *sql.Tx, sqlGen *sqlutil.SQLGen) (
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = InsertTableBySQLGenTx(tx, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Insert()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -110,7 +181,10 @@ func (db *DBWrapper) DeleteTableBySQLGen(sqlGen *sqlutil.SQLGen) (result int64, 
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = DeleteTableBySQLGen(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Delete()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -120,7 +194,10 @@ func (db *DBWrapper) DeleteTableBySQLGenTx(tx *sql.Tx, sqlGen *sqlutil.SQLGen) (
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = DeleteTableBySQLGenTx(tx, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Delete()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -130,7 +207,10 @@ func (db *DBWrapper) UpdateTableBySQLGen(sqlGen *sqlutil.SQLGen) (result int64, 
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = UpdateTableBySQLGen(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Update()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -140,7 +220,10 @@ func (db *DBWrapper) UpdateTableBySQLGenTx(tx *sql.Tx, sqlGen *sqlutil.SQLGen) (
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = UpdateTableBySQLGenTx(tx, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Update()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -150,7 +233,7 @@ func (db *DBWrapper) Exec(sqlStr string, args ...interface{}) (result int64, err
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = Exec(db.OriginDB, sqlStr, args...)
-	db.check(t, sqlStr)
+	db.checkSQLStr(t, sqlStr)
 	return
 }
 
@@ -160,7 +243,9 @@ func (db *DBWrapper) ExecTx(tx *sql.Tx, sqlStr string, args ...interface{}) (res
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = ExecTx(tx, sqlStr, args...)
-	db.check(t, sqlStr)
+	if db.openCheckSlowSQL {
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -170,7 +255,10 @@ func (db *DBWrapper) GetRowBySQLGen(sqlGen *sqlutil.SQLGen) (result []string, er
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowBySQLGen(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Query()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -180,7 +268,10 @@ func (db *DBWrapper) GetRowBySQLGenTx(tx *sql.Tx, sqlGen *sqlutil.SQLGen) (resul
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowBySQLGenTx(tx, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Query()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -190,7 +281,9 @@ func (db *DBWrapper) GetRowBySQLStr(sqlStr string, args ...interface{}) (result 
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowBySQLStr(db.OriginDB, sqlStr, args...)
-	db.check(t, sqlStr)
+	if db.openCheckSlowSQL {
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -200,7 +293,10 @@ func (db *DBWrapper) GetRowsBySQLGenPrintSql(sqlGen *sqlutil.SQLGen) (result [][
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowsBySQLGenPrintSql(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Query()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -210,7 +306,10 @@ func (db *DBWrapper) GetRowsBySQLGen(sqlGen *sqlutil.SQLGen) (result [][]string,
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowsBySQLGen(db.OriginDB, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Query()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -220,7 +319,10 @@ func (db *DBWrapper) GetRowsBySQLGenTx(tx *sql.Tx, sqlGen *sqlutil.SQLGen) (resu
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowsBySQLGenTx(tx, sqlGen)
-	db.check(t, sqlGen)
+	if db.openCheckSlowSQL {
+		sqlStr, _, _ := sqlGen.Query()
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -230,7 +332,9 @@ func (db *DBWrapper) GetRowsBySQLStr(sqlStr string, args ...interface{}) (result
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowsBySQLStr(db.OriginDB, sqlStr, args...)
-	db.check(t, sqlStr)
+	if db.openCheckSlowSQL {
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }
 
@@ -240,6 +344,8 @@ func (db *DBWrapper) GetRowsBySQLStrWithQueryColumnsCount(sqlStr string, queryCo
 		t = stopwatch.NewStopWatch(slowSQLCheck)
 	}
 	result, err = GetRowsBySQLStr(db.OriginDB, sqlStr)
-	db.check(t, sqlStr)
+	if db.openCheckSlowSQL {
+		db.checkSQLStr(t, sqlStr)
+	}
 	return
 }

91943558a6bb8893b124246a62b8c2cb0df2bed7
David <admin@dxscx.com>
2019-06-14 10:47:10 +0800

    chore: backup

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -1,6 +1,7 @@
-package testutil
+package integrationtest
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/sssvip/goutil/strutil"
 	"github.com/sssvip/goutil/timeutil"
@@ -14,10 +15,16 @@ import (
 
 var successChar = clcolor.Green("✔️")
 var failedChar = clcolor.Red("✖️")
-var swAll = stopwatch.NewStopWatch("swAll")
+var swAll *stopwatch.StopWatch
 var stepCount = 0
 var allShowTryTextLen = 0
 var backupTryTimeNum = 0
+
+var addBlankSpaceInWordSignal bool
+
+func AddBlankSpaceInWord(s bool) {
+	addBlankSpaceInWordSignal = s
+}
 
 func init() {
 	if IsWindows() {
@@ -51,14 +58,38 @@ func ShowTryText(time int) {
 	backupTryTimeNum = time
 	allShowTryTextLen += utf8.RuneCountInString(str)
 	fmt.Print(clcolor.Yellow(str))
+	//PrintLine(clcolor.Yellow(str))
 }
 
 func PrintLine(text string) {
 	Print(text + "\n")
 }
 
+func AddBlankSpace(text string) string {
+	buf := bytes.NewBufferString("")
+	chars := []rune(text)
+	lastIsGT256 := false
+	for _, x := range chars {
+		if x > 256 { // just for termtosvg more readable
+			buf.WriteString(" ")
+			lastIsGT256 = true
+			buf.WriteString(string(x))
+			continue
+		}
+		if lastIsGT256 {
+			buf.WriteString(" ")
+			lastIsGT256 = false
+		}
+		buf.WriteString(string(x))
+	}
+	return buf.String()
+}
+
 func Print(text string) {
 	ClearShowTry()
+	if addBlankSpaceInWordSignal {
+		text = AddBlankSpace(text)
+	}
 	fmt.Print(text)
 	ReShowTryTimes()
 }
@@ -73,16 +104,20 @@ func ReShowTryTimes() {
 func ClearShowTry() {
 	BackTimes(allShowTryTextLen)
 	allShowTryTextLen = 0
+	backupTryTimeNum = 0
 }
 
 func TryMoreTime(f func() error, times int, name string, periodPerExecMill ...int) {
+	if swAll == nil {
+		swAll = stopwatch.NewStopWatch("swAll")
+	}
 	periodPerExecReal := 1000
 	if len(periodPerExecMill) > 0 {
 		periodPerExecReal = periodPerExecMill[0]
 	}
 	stepCount++
 	sw := stopwatch.NewStopWatch("t")
-	fmt.Print(strutil.Format("%d.[%s]\n", stepCount, name))
+	PrintLine(strutil.Format("%d.[%s]", stepCount, name))
 	err := f()
 	tryTimes := 1
 	for {
@@ -106,20 +141,22 @@ func TryMoreTime(f func() error, times int, name string, periodPerExecMill ...in
 	}
 	useTimeStr := strutil.Format("current step use %dms(%d %s),total use %ds", sw.ElapsedMilliSeconds(), tryTimes, timeStr, swAll.ElapsedSeconds())
 	if err != nil {
-		fmt.Println(strutil.Format("current step [%s] test result: %s", name, failedChar))
-		fmt.Println(strutil.Format("Failed reason:[%s], please check...", RedErrorStr(err.Error())))
-		fmt.Println(useTimeStr)
+
+		PrintLine(strutil.Format("current step [%s] test result: %s", name, failedChar))
+		PrintLine(strutil.Format("Failed reason:[%s], please check...", RedErrorStr(err.Error())))
+		PrintLine(useTimeStr)
 		os.Exit(-1)
 	} else {
-		fmt.Println(strutil.Format("current step [%s] test result: %s", name, successChar))
-		fmt.Println(useTimeStr)
+		PrintLine(strutil.Format("current step [%s] test result: %s", name, successChar))
+		PrintLine(useTimeStr)
 	}
 }
 
 func StartTest() {
-	fmt.Println(strutil.Format("start to test all,now:%s", timeutil.FormatDateTimeString(time.Now())))
+	swAll = stopwatch.NewStopWatch("swAll")
+	PrintLine(strutil.Format("start to test all,now:%s", timeutil.FormatDateTimeString(time.Now())))
 }
 func EndTest() {
 	endTimeStr := timeutil.FormatDateTimeString(time.Now())
-	fmt.Println(strutil.Format("pass all tests... total use %ds, end time:%s", swAll.ElapsedSeconds(), endTimeStr))
+	PrintLine(strutil.Format("pass all tests... total use %ds, end time:%s", swAll.ElapsedSeconds(), endTimeStr))
 }
